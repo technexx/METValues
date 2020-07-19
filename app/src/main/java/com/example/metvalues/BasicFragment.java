85269f0988eef6cceb2cb7695c8f15f62cf4a2a4
technexx <48839830+technexx@users.noreply.github.com>
2020-07-18 20:38:16 -0700

    tied met values to subcat spinner

--- app/src/main/java/com/example/metvalues/BasicFragment.java
+++ app/src/main/java/com/example/metvalues/BasicFragment.java
@@ -172,5 +172,4 @@ public class BasicFragment extends Fragment {
         });
         return root;
     }
-
 }
--- app/src/main/java/com/example/metvalues/MetFragment.java
+++ app/src/main/java/com/example/metvalues/MetFragment.java
@@ -11,6 +11,7 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -27,9 +28,8 @@ public class MetFragment extends Fragment {
 
     private ArrayAdapter subCategoryAdapter;
     private List<String> total_list;
-
-
     private boolean isMetric;
+    private int catNumber;
 
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
 
@@ -39,6 +39,9 @@ public class MetFragment extends Fragment {
         List<String> category_list = new ArrayList<>();
         List<String> activity_list = new ArrayList<>();
         List<String> score_list = new ArrayList<>();
+        List<Double> hours_list = new ArrayList<>();
+        final TextView met_score = root.findViewById(R.id.met_score);
+        double hours = 0;
 
         category_list.add(getString(R.string.bicycling));
         category_list.add(getString(R.string.conditioning));
@@ -93,7 +96,8 @@ public class MetFragment extends Fragment {
 
         final Spinner weight_spinner = root.findViewById(R.id.met_spinnerOne);
         final Spinner category_spinner = root.findViewById(R.id.met_spinnerTwo);
-        final  Spinner sub_category_spinner = root.findViewById(R.id.met_spinnerThree);
+        final Spinner sub_category_spinner = root.findViewById(R.id.met_spinnerThree);
+        final Spinner hours_spinner = root.findViewById(R.id.hours_spent);
 
         Bundle args = getArguments();
         if (args != null) {
@@ -110,14 +114,23 @@ public class MetFragment extends Fragment {
             }
         }
 
+        for (int i=0; i<48; i++) {
+            hours = hours + 0.5;
+            hours_list.add(hours);
+        }
+        Log.i("hours", String.valueOf(hours_list));
+
         ArrayAdapter<String> weightAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner, weight_list);
         ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner, category_list);
+        ArrayAdapter<Double> hoursAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner, hours_list);
 
         weightAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hoursAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         weight_spinner.setAdapter(weightAdapter);
         category_spinner.setAdapter(categoryAdapter);
+        hours_spinner.setAdapter(hoursAdapter);
 
         category_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -125,55 +138,116 @@ public class MetFragment extends Fragment {
                 switch (category_spinner.getSelectedItemPosition()) {
                     case 0:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, bicycling);
+                        catNumber = 0;
                         break;
                     case 1:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, conditioning);
+                        catNumber = 1;
                         break;
                     case 2:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, dancing);
+                        catNumber = 2;
                         break;
                     case 3:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, fishing);
+                        catNumber = 3;
                         break;
                     case 4:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, home);
+                        catNumber = 4;
                         break;
                     case 5:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, garden);
+                        catNumber = 5;
                         break;
                     case 6:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, misc);
+                        catNumber = 6;
                         break;
                     case 7:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, music);
+                        catNumber = 7;
                         break;
                     case 8:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, occupational);
+                        catNumber = 8;
                         break;
                     case 9:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, running);
+                        catNumber = 9;
                         break;
                     case 10:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, self_care);
+                        catNumber = 10;
                         break;
                     case 11:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, sexual);
+                        catNumber = 11;
                         break;
                     case 12:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, sports);
+                        catNumber = 12;
                         break;
                     case 13:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, water);
+                        catNumber = 13;
                         break;
                     case 14:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, walking);
+                        catNumber = 14;
                         break;
                     case 15:
                         subCategoryAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_two, winter);
+                        catNumber = 15;
                 }
                 subCategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 sub_category_spinner.setAdapter(subCategoryAdapter);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        sub_category_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            int pos = sub_category_spinner.getSelectedItemPosition();
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (catNumber) {
+                    case 0:
+                        met_score.setText(bicycling_values[pos]); break;
+                    case 1:
+                        met_score.setText(conditioning_values[pos]); break;
+                    case 2:
+                        met_score.setText(dancing_values[pos]); break;
+                    case 3:
+                        met_score.setText(fishing_values[pos]); break;
+                    case 4:
+                        met_score.setText(home_values[pos]); break;
+                    case 5:
+                        met_score.setText(garden_values[pos]); break;
+                    case 6:
+                        met_score.setText(misc_values[pos]); break;
+                    case 7:
+                        met_score.setText(music_values[pos]); break;
+                    case 8:
+                        met_score.setText(occupational_values[pos]); break;
+                    case 9:
+                        met_score.setText(running_values[pos]); break;
+                    case 10:
+                        met_score.setText(self_care_values[pos]); break;
+                    case 11:
+                        met_score.setText(sexual_values[pos]); break;
+                    case 12:
+                        met_score.setText(sports_values[pos]); break;
+                    case 13:
+                        met_score.setText(walking_values[pos]); break;
+                    case 14:
+                        met_score.setText(water_values[pos]); break;
+                    case 15:
+                        met_score.setText(winter_values[pos]);
+                }
             }
 
             @Override
