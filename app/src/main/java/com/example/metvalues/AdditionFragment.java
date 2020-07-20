d58da92206624550c5e2ae0df019e281a3856537
technexx <48839830+technexx@users.noreply.github.com>
2020-07-20 12:47:48 -0700

    added stupid imagebutton since fab was being terrible

--- app/src/main/java/com/example/metvalues/AdditionFragment.java
+++ app/src/main/java/com/example/metvalues/AdditionFragment.java
@@ -7,11 +7,11 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public class CustomFragment extends Fragment {
+public class AdditionFragment extends Fragment {
 
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.custom_fragment, container, false);
+        View root = inflater.inflate(R.layout.addition_fragment, container, false);
 
         return root;
 
